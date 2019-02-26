fa0a9fc7052f5d02def1c53b092a118e44024cd1
fanj37 <fanj37@mcmaster.ca>
2019-02-26 22:49:36 +0000

    Part2

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -10,3 +10,30 @@ if [[ "$1" == "6" ]]
 		done
 fi
 
+if [[ "$1" == "2" ]]
+	then
+		grep -nr “#TODO” ~/CS1XA3 > ~/CS1XA3/Project01/todo.log
+
+fi
+
+
+if [[ "$1" == "4" ]]
+	then
+		{
+			rm -f "merge.log"
+			log=`git log --oneline`
+			while read -r line; do
+				if [[ ! -z `echo $line | grep "Merge"` ]]; then
+					goal=`echo $line | cut -d " " -f 1`
+					echo $goal >> merge.log
+					echo "$goal"
+				fi
+			done <<< $log
+		}
+fi
+
+
+if [[ "$1" == "ex" ]]
+	then
+		ifconfig | grep "inet addr:" | awk '{ print $2 }' | sed 's/addr://g'
+fi
